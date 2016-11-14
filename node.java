30145a288638a2c4839c39342a7e5f9240631e82
DESKTOP-UGN8TA5\Emoney <ethornton@albany.edu>
2016-11-13 22:30:52 -0500

    Making the node class for the sorting program

---
+++ node.java
@@ -0,0 +1,17 @@
+public class node
+    {
+       //setting the int to data
+        int data;
+        //making the node next
+        Node next;
+        
+        //Node Constructor
+        Node(int d)
+        {
+          //setting data to d
+            data = d;
+            //making the next node null
+            next = null;
+        }
+        
+    }
