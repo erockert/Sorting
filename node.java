cec69172c3335cce3acf9ba3d7aa65d92e5af078
DESKTOP-UGN8TA5\Emoney <ethornton@albany.edu>
2016-11-13 22:34:17 -0500

    Finished my node class

--- node.java
+++ node.java
@@ -3,10 +3,10 @@ public class node
        //setting the int to data
         int data;
         //making the node next
-        Node next;
+        node next;
         
         //Node Constructor
-        Node(int d)
+      node(int d)
         {
           //setting data to d
             data = d;
